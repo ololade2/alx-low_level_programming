436f53b19ae36a4cee2d2bf8c3064a94197ba893
ololade2 <ogunsolaololade@gmail.com>
2023-10-12 03:30:50 +0100

    print

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,7 +15,7 @@ void print_to_98(int n)
 		{
 			printf("%d", i);
 			if (i != 98)
-				printf(", ");
+				printf(" , ");
 		}
 		printf("\n");
 	}
@@ -25,17 +25,17 @@ void print_to_98(int n)
 		{
 			printf("%d", i);
 			if (i != 98)
-				printf(",");
+				printf(" , ");
 		}
 		printf("\n");
 	}
-	else if (n >= 98)
+	else if (n > 98)
 	{
 		for (i = n ; i >= 98 ; i--)
 		{
 			printf("%d", i);
 			if (i != 98)
-				printf(", ");
+				printf(" , ");
 		}
 		printf("\n");
 	}
